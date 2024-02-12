b0ca4640e1d09dce9df7229b14215239bb8527ad
omkarshinde254 <omkarshinde254@gmail.com>
2024-02-12 12:31:54 -0500

    made minor modifications

--- users/views.py
+++ users/views.py
@@ -22,7 +22,7 @@ class RegisterView(APIView):
         
         if serializer.is_valid():
             serializer.save()
-            return Response({'data': {serializer.dta}}, status=status.HTTP_201_CREATED)
+            return Response({'data': {serializer.data}}, status=status.HTTP_201_CREATED)
         else:
             return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
