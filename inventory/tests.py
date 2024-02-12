2b7d288700196ec1a2035a3033d07f8860281b12
omkarshinde254 <omkarshinde254@gmail.com>
2024-02-12 16:19:49 -0500

    addes test cases

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+from .models import Category, Item
+import json
 
-# Create your tests here.
+class CategoryItemTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.category_count_url = reverse('category-count')
+        self.item_count_url = reverse('item-count')
+        self.category_create_url = reverse('category-create')
+        self.category_data = {
+            "name": "test_category",
+        }
+        self.item_create_url = reverse('item-create')
+        self.category_list_url = reverse('category-list')
+        self.item_list_url = reverse('item-list')
+        self.item_data = {
+            "sku": "test_sku",
+            "name": "test_item",
+            "tags": "test_tags",
+            "category": "test_category",
+            "in_stock": "yes",
+            "available": "yes"
+        }
+
+    def test_category_count(self):
+        response = self.client.get(self.category_count_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_item_count(self):
+        response = self.client.get(self.item_count_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_category(self):
+        response = self.client.post(self.category_create_url, self.category_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Category.objects.count(), 1)
+        self.assertEqual(Category.objects.get().name, 'test_category')
+
+    # def test_create_item(self):
+    #     response = self.client.post(self.category_create_url, self.category_data, format='json')
+    #     response = self.client.post(self.item_create_url, self.item_data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     self.assertEqual(Item.objects.count(), 1)
+    #     self.assertEqual(Item.objects.get().sku, 'test_sku')
+
+
+    def test_category_list(self):
+        response = self.client.get(self.category_list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def test_item_list(self):
+    #     self.test_create_item()  # Create an item first
+    #     response = self.client.get(self.item_list_url)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+from .models import User
+import json
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.user_data = {
+            "username": "testuser@example.com",
+            "password": "testpassword"
+        }
+
+    def test_register_user(self):
+        response = self.client.post(self.register_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'testuser@example.com')
+
+    def test_login_user(self):
+        self.test_register_user()  # Register a user first
+        response = self.client.post(self.login_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('jwt' in json.loads(response.content)['data'])
+
+    def test_login_user_wrong_password(self):
+        self.test_register_user()  # Register a user first
+        self.user_data['password'] = 'wrongpassword'  # Use wrong password
+        response = self.client.post(self.login_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
