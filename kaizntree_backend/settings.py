831f112ca53c1fdf3216cadd6cdac1ea1269b243
omkarshinde254 <omkarshinde254@gmail.com>
2024-02-12 20:54:24 -0500

    updated settings

--- kaizntree_backend/settings.py
+++ kaizntree_backend/settings.py
@@ -72,11 +72,7 @@ TEMPLATES = [
     },
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
-}
+
 
 WSGI_APPLICATION = 'kaizntree_backend.wsgi.application'
 
